986745a22f4da5e37370e0b521e4522e835727db
Aki Unleash <aki.unleash@gmai.comm>
2021-03-27 09:08:12 +0900

    Initial value setting.

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux'
 // Component
 import Header from '../organisms/Header_in';
 import Profileform from '../organisms/Profileform';
 // common
 import { loginChack, authenticatedChack } from "../../common/backend/model"
+// state
+import { selectUser } from "../../common/state/userSlice"
 
 const Profileedit: React.FC = () => {
+  const user = useSelector(selectUser)
 
   useEffect(() => {
     // ログイン済みの確認
@@ -17,7 +21,7 @@ const Profileedit: React.FC = () => {
   return (
     <>
       <Header />
-      <Profileform />
+      {user.uid ? <Profileform /> : ""}
     </>
   );
 };
