a24b239d8156e178920d1af574ef39668840250b
Aki Unleash <aki.unleash@gmai.comm>
2021-04-02 13:54:11 +0900

    Fixed to show the diary of all followers.

--- src/components/organisms/Diarylist.tsx
+++ src/components/organisms/Diarylist.tsx
@@ -5,6 +5,7 @@ import { db } from '../../common/firebase/firebase'
 import Diarycard from '../Molecules/Diarycard'
 // until
 import { selectUser } from "../../common/state/userSlice"
+import { any } from 'prop-types';
 
 const Diarylist: React.FC = () => {
   const user = useSelector(selectUser);
@@ -25,7 +26,8 @@ const Diarylist: React.FC = () => {
 
   // フォロワーの日記データ取得
   useEffect(() => {
-    user.follower.forEach(async (follower) => {
+    let posts: any = []
+    user.follower.forEach(async (follower, index, array) => {
       const f = await db.collection("user").doc(follower).get()
       const nickname = f.data()?.nickname
       const avatarUrl = f.data()?.avatarurl
@@ -36,20 +38,30 @@ const Diarylist: React.FC = () => {
         .collection("posts")
         .orderBy("create_at", "desc")
         .onSnapshot((snapshot) => {
-          setPost(
-            snapshot.docs.map((doc) => ({
-              id: doc.id,
-              title: doc.data().title,
-              body: doc.data().body,
-              gametitle: doc.data().gamename,
-              link: '/' + profileID + '/status/' + doc.id,
-              nicecount: doc.data().nicecount,
-              displayName: nickname,
-              avatarUrl: avatarUrl,
-              attachUrl: doc.data().attachimage,
-              create_at: `${doc.data().create_at.toDate().getFullYear()}/${("00" + (doc.data().create_at.toDate().getMonth() + 1)).slice(-2)}/${("00" + doc.data().create_at.toDate().getDate()).slice(-2)}`,
-            }))
-          )
+          const p = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            title: doc.data().title,
+            body: doc.data().body,
+            gametitle: doc.data().gamename,
+            link: '/' + profileID + '/status/' + doc.id,
+            nicecount: doc.data().nicecount,
+            displayName: nickname,
+            avatarUrl: avatarUrl,
+            attachUrl: doc.data().attachimage,
+            create_at: `${doc.data().create_at.toDate().getFullYear()}/${("00" + (doc.data().create_at.toDate().getMonth() + 1)).slice(-2)}/${("00" + doc.data().create_at.toDate().getDate()).slice(-2)}`,
+          }))
+
+          // Data accumulation
+          posts.push(...p)
+          if ((index + 1) === array.length) {
+            // Sort 
+            posts.sort((a: any, b: any) => {
+              if (a.create_at > b.create_at) {
+                return -1;
+              } else { return 1; }
+            })
+            setPost(posts)
+          }
         }
         );
     })
