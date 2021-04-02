7fcca59b9a13a83be18bbe8a398d2dc33e208981
Aki Unleash <aki.unleash@gmai.comm>
2021-04-02 10:04:47 +0900

    Implement local emulator.

---
+++ webpack.dev_local_emulator.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MODE = "development";
+const Dotenv = require('dotenv-webpack');
+
+module.exports = merge(common, {
+  mode: MODE,
+  devtool: 'inline-source-map',
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename]
+    }
+  },
+  plugins: [
+    // .envを使用するプラグイン
+    new Dotenv({
+      path: './.env_local_emulator'
+    }
+    ),
+  ]
+});
