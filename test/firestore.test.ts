b41bbc4fc8f94f9262c5474a9fc3332234dfd8b2
Aki Unleash <aki.unleash@gmai.comm>
2021-04-05 16:02:00 +0900

    Add security rules(nices)

--- test/firestore.test.ts
+++ test/firestore.test.ts
@@ -414,3 +414,99 @@ describe('/user/{uid}/posts/{uid}/comments/{random}', () => {
     })
   })
 })
+
+
+
+describe('/user/{uid}/posts/{uid}/nices/{random}', () => {
+
+  type nices_type = {
+    create_at: any,
+    userID: any,
+  }
+
+  const testdata_nices: nices_type = {
+    create_at: firebase.firestore.FieldValue.serverTimestamp(),
+    userID: randomID().slice(0, 28),
+  }
+
+  describe('Create', () => {
+    describe('正常系', () => {
+
+      const userID = randomID().slice(0, 28)
+      const db = clientDB({ uid: userID })
+
+      it('ログインユーザーが正常データの登録', async () => {
+        await firebase.assertSucceeds(db
+          .collection('user')
+          .doc(userID)
+          .collection('posts')
+          .doc(randomID())
+          .collection('nices')
+          .doc(randomID())
+          .set(testdata_nices)
+        )
+      })
+    })
+
+    describe('異常系', () => {
+
+      it('ログインせずに正常データの登録', async () => {
+        const userID = randomID().slice(0, 28)
+        const db = clientDB()
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('posts')
+          .doc(randomID())
+          .collection('nices')
+          .doc(randomID())
+          .set(testdata_nices)
+        )
+      })
+
+      const userID = randomID().slice(0, 28)
+      const db = clientDB({ uid: userID })
+
+      it('必須項目が抜けている（ユーザーID)', async () => {
+        const { userID, ...testdata } = testdata_nices
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('posts')
+          .doc(randomID())
+          .collection('nices')
+          .doc(randomID())
+          .set(testdata)
+        )
+      })
+
+      it('必須項目が抜けている（登録日時)', async () => {
+        const { create_at, ...testdata } = testdata_nices
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('posts')
+          .doc(randomID())
+          .collection('nices')
+          .doc(randomID())
+          .set(testdata)
+        )
+      })
+
+      it('登録日に文字列を挿入', async () => {
+        const { ...testdata } = testdata_nices
+        testdata.create_at = "2021/04/01"
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('posts')
+          .doc(randomID())
+          .collection('nices')
+          .doc(randomID())
+          .set(testdata)
+        )
+      })
+
+    })
+  })
+})
