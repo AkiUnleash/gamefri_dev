3c8c18d28613b82a76d8bfdad3b3027b6ab05494
Aki Unleash <aki.unleash@gmai.comm>
2021-04-06 14:57:30 +0900

    Add animation to top page.

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { D useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import styles from '../../assets/scss/top.module.scss';
 // Component
 import Header from '../organisms/Header';
@@ -12,7 +13,8 @@ import detail_img_3 from '../../assets/images/top/top-img3.png'
 import { loginChack_done } from "../../common/backend/model"
 
 
-const detail = (): JSX.Element => {
+const Detail1 = (): JSX.Element => {
+  const { ref, inView } = useInView({ rootMargin: '-50% 0px', triggerOnce: true });
 
   const detail_data = [
     {
@@ -21,12 +23,54 @@ const detail = (): JSX.Element => {
       image: detail_img_1,
       style: "top-detail__figure"
     },
+  ]
+
+  const items = detail_data.map((item, key) => {
+    return (
+      <section ref={ref} className={styles["top-detail"]} key={key}>
+        <figure className={styles[item.style]}><img className={inView ? styles["top-detail__figure-img"] : styles['none']} src={item.image} alt="" />
+          <figcaption className={styles["top-detail__figcaption"]}>
+            <div className={inView ? styles["top-detail__title"] : styles['none']}>{item.title}</div>
+            <div className={inView ? styles["top-detail__discription"] : styles['none']}>{item.disctiption}</div>
+          </figcaption>
+        </figure>
+      </section >
+    )
+  });
+  return (<>{items}</>);
+};
+
+const Detail2 = (): JSX.Element => {
+  const { ref, inView } = useInView({ rootMargin: '-40% 0px', triggerOnce: true });
+
+  const detail_data = [
     {
       title: "約束をして協力プレイ！！",
       disctiption: "同じ時間帯にゲームをするフレンドを誘って一緒にゲームをしよう！",
       image: detail_img_2,
       style: "top-detail__figure_reversai"
     },
+  ]
+
+  const items = detail_data.map((item, key) => {
+    return (
+      <section ref={ref} className={styles["top-detail"]} key={key}>
+        <figure className={styles[item.style]}><img className={inView ? styles["top-detail__figure-img"] : styles['none']} src={item.image} alt="" />
+          <figcaption className={styles["top-detail__figcaption"]}>
+            <div className={inView ? styles["top-detail__title"] : styles['none']}>{item.title}</div>
+            <div className={inView ? styles["top-detail__discription"] : styles['none']}>{item.disctiption}</div>
+          </figcaption>
+        </figure>
+      </section >
+    )
+  });
+  return (<>{items}</>);
+};
+
+const Detail3 = (): JSX.Element => {
+  const { ref, inView } = useInView({ rootMargin: '-30% 0px', triggerOnce: true });
+
+  const detail_data = [
     {
       title: "楽しかったことを皆にシェア！",
       disctiption: "ゲーム日記をつけることができます。皆からコメントを貰って、フレンドを増やそう！",
@@ -37,11 +81,11 @@ const detail = (): JSX.Element => {
 
   const items = detail_data.map((item, key) => {
     return (
-      <section className={styles["top-detail"]} key={key}>
-        <figure className={styles[item.style]}><img className={styles["top-detail__figure-img"]} src={item.image} alt="" />
+      <section ref={ref} className={styles["top-detail"]} key={key}>
+        <figure className={styles[item.style]}><img className={inView ? styles["top-detail__figure-img"] : styles['none']} src={item.image} alt="" />
           <figcaption className={styles["top-detail__figcaption"]}>
-            <div className={styles["top-detail__title"]}>{item.title}</div>
-            <div className={styles["top-detail__discription"]}>{item.disctiption}</div>
+            <div className={inView ? styles["top-detail__title"] : styles['none']}>{item.title}</div>
+            <div className={inView ? styles["top-detail__discription"] : styles['none']}>{item.disctiption}</div>
           </figcaption>
         </figure>
       </section >
@@ -52,17 +96,18 @@ const detail = (): JSX.Element => {
 
 
 
-const massage = (): JSX.Element => {
+const Massage = (): JSX.Element => {
+  const { ref, inView } = useInView({ rootMargin: '-20% 0px', triggerOnce: true });
   const title_why: string = "ゲムフレとは？"
   const title_discription: string[] =
     ["「 世界中のみんなと協力してゲームをしたい！ 」",
       "ゲーム専用のソーシャルネットワークサービスです。"]
 
   return (
-    <section className={styles["top-massage"]}>
-      <div className={styles["top-massage__title-why"]}>{title_why}</div>
-      <div className={styles["top-massage__title-discription"]}>{title_discription[0]}</div>
-      <div className={styles["top-massage__title-discription"]}>{title_discription[1]}</div>
+    <section ref={ref} className={styles["top-massage"]}>
+      <div className={inView ? styles["top-massage__title-why"] : styles['none']}>{title_why}{console.log(`${inView}`)}</div>
+      <div className={inView ? styles["top-massage__title-discription"] : styles['none']}>{title_discription[0]}</div>
+      <div className={inView ? styles["top-massage__title-discription"] : styles['none']}>{title_discription[1]}</div>
     </section>
   );
 }
@@ -96,8 +141,10 @@ const Top: React.FC = () => {
     <>
       <Header />
       {introduction()}
-      {massage()}
-      {detail()}
+      {Massage()}
+      {Detail1()}
+      {Detail2()}
+      {Detail3()}
       <Footer />
     </>
   );
