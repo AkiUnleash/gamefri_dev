7dd6869c20faca3e6226f839821691709fb015b2
Aki Unleash <aki.unleash@gmai.comm>
2021-04-09 15:57:15 +0900

    Add the notification rule to the security rule.

--- test/firestore_followings.test.ts
+++ test/firestore_followings.test.ts
@@ -18,16 +18,22 @@ afterAll(async () => {
 })
 
 
-describe('/user/{uid}/followings/{uid}', () => {
+describe('/user/{uid}/notification/{id}', () => {
 
-  type followings_type = {
+  type notifications_type = {
+    avatarurl: any,
     create_at: any,
-    userID: any,
+    message: any,
+    nickname: any
+    profileid: any,
   }
 
-  const testdata_followings: followings_type = {
+  const testdata_notifications: notifications_type = {
+    avatarurl: "http://example.com/avatarurl",
     create_at: firebase.firestore.FieldValue.serverTimestamp(),
-    userID: randomID().slice(0, 28)
+    message: "message/message",
+    nickname: "nickname",
+    profileid: "profileid"
   }
 
   describe('Create', () => {
@@ -40,9 +46,9 @@ describe('/user/{uid}/followings/{uid}', () => {
         await firebase.assertSucceeds(db
           .collection('user')
           .doc(userID)
-          .collection('followings')
-          .doc(testdata_followings.userID)
-          .set(testdata_followings)
+          .collection('notifications')
+          .doc(randomID())
+          .set(testdata_notifications)
         )
       })
     })
@@ -55,32 +61,63 @@ describe('/user/{uid}/followings/{uid}', () => {
         await firebase.assertFails(db
           .collection('user')
           .doc(userID)
-          .collection('followings')
-          .doc(testdata_followings.userID)
-          .set(testdata_followings))
+          .collection('notifications')
+          .doc(randomID())
+          .set(testdata_notifications)
+        )
       })
 
       const userID = randomID().slice(0, 28)
       const db = clientDB({ uid: userID })
 
       it('必須項目が抜けている（登録日時)', async () => {
-        const { create_at, ...testdata } = testdata_followings
+        const { create_at, ...testdata } = testdata_notifications
         await firebase.assertFails(db
           .collection('user')
           .doc(userID)
-          .collection('followings')
-          .doc(testdata_followings.userID)
+          .collection('notifications')
+          .doc(randomID())
+          .set(testdata))
+      })
+
+      it('必須項目が抜けている（メッセージ)', async () => {
+        const { message, ...testdata } = testdata_notifications
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('notifications')
+          .doc(randomID())
+          .set(testdata))
+      })
+
+      it('必須項目が抜けている（ニックネーム)', async () => {
+        const { nickname, ...testdata } = testdata_notifications
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('notifications')
+          .doc(randomID())
+          .set(testdata))
+      })
+
+      it('必須項目が抜けている（ユーザーID)', async () => {
+        const { profileid, ...testdata } = testdata_notifications
+        await firebase.assertFails(db
+          .collection('user')
+          .doc(userID)
+          .collection('notifications')
+          .doc(randomID())
           .set(testdata))
       })
 
       it('登録日に文字列を挿入', async () => {
-        const { ...testdata } = testdata_followings
+        const { ...testdata } = testdata_notifications
         testdata.create_at = "2021/04/01"
         await firebase.assertFails(db
           .collection('user')
           .doc(userID)
-          .collection('followings')
-          .doc(testdata_followings.userID)
+          .collection('notifications')
+          .doc(randomID())
           .set(testdata))
       })
     })
